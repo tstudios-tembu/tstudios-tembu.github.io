e953afcaa730a3d5f55da60e17603bc383d13402
oeggy03 <oeggy03>
2023-10-19 14:54:04 +0800

    Create navbar and logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/navbar/navbar"
 
 function App() {
   return (
-    <>
-      <div>
-      </div>
-    </>
+    <div className='App'>
+      <Navbar/>
+    </div>
   )
 }
 
